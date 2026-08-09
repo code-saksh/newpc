43b7535ed0c233d6074cd5ddccdcd9770afefecb
Saksham Agarwal <bcskolkata!@gmail.com>
2026-08-09 15:12:11 +0530

    Dulpicate items

--- python/test.py
+++ python/test.py
@@ -1,12 +1,12 @@
 
+items = [1,2,2,3,3,3,4,5,7,7,9,9,10]
+dup = [] 
+for i in  items :
+    count=0
+    for j in items : 
+        if i == j :
+            count = count +1 
+    if count > 1 and i not in dup :
+                dup.append(i)
 
-for i in range(1,51):
-    if i%3==0 and i%5==0:
-        print("FizzBuzz")
-    elif i%3==0:
-        print("Fizz")
-    elif i%5==0:
-        print("Buzz")
-    else:
-        print(i)
-    
+print(dup)
