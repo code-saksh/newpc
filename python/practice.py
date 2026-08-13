10f702d0f376ce1813bc466ac24a6207b06e82ef
Saksham Agarwal <bcskolkata!@gmail.com>
2026-08-13 14:58:02 +0530

    min and max value

---
+++ python/practice.py
@@ -0,0 +1,6 @@
+
+a=50
+b=60
+c=90
+print(max(a,b,c))
+print(min(a,b,c))
--- python/test.py
+++ python/test.py
@@ -1,4 +1,23 @@
 
-prices={'pen':10,'book':50,'bag':200}
-for i in prices.items():
-    print(f'{i[0]} costs {i[1]}')
+
+dic={ "dic1" : { "number" : 7003092759 , "email" : "bcskolkata1@gmail.com", "city" : "bangalore" } , "dic2" : { "number" : 8100282789 , "email" : "bcskolkata@gmail.com" , "city" : "kolkata" }} 
+
+dic["dic1"]["number"] = 7003092750 
+print(dic)
+del dic["dic2"]["number"]
+print(dic)
+
+for i in dic.items():
+    print(i)
+
+a=int(input("Enter a number : "))
+found = False
+for i in dic.values():
+    if i.get("number") == a:
+        print(i["email"])
+        print(i["city"])
+        found = True
+        break
+if not found:
+    print("No record found for that number")
+
