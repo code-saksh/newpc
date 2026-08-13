d6fa9a02a2ad9a404baf11457d11285ac81b7f71
Saksham Agarwal <bcskolkata!@gmail.com>
2026-08-13 15:41:31 +0530

    number of odd and even digits in a number n

--- practice/practice.py
+++ practice/practice.py
@@ -1,10 +1,16 @@
 
 a=int(input("Enter a number : "))
 c=a
-rev=""
+count =0 
+count1=0
 while a>0:
     b=a%10
-    rev=rev+str(b)
+    if b%2==0:
+        count= count +1 
+    else: 
+        count1=count1+1
+
     a=a//10
 
-print(rev)
+print("odd digits" , count1)
+print("even digits" , count)
