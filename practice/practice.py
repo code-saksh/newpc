2dcf82043534650bf406817ee2001044cf4a2e2c
Saksham Agarwal <bcskolkata!@gmail.com>
2026-08-14 17:28:17 +0530

    Automorphic number

--- practice/practice.py
+++ practice/practice.py
@@ -1,17 +1,10 @@
 
 a=int(input("Enter a number : "))
-b=a
-sum=0
-
-while a>0:
-    sum1=1
-    c=a%10
-    for i in range(1,c+1):
-        sum1=sum1*i
-    sum=sum+sum1
-    a=a//10
-
-if sum==b:
-    print("Strong number")
+b=str(a)
+c=len(b)
+d=a*a
+e=10**c
+if d%e==a:
+    print("Automorphic number")
 else:
-    print("Not a Strong number")
+    print("Not an Automorphic number")
